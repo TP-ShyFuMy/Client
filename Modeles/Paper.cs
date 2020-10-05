398d4adaf490a18c4a2c29777f2443ee5f00f9b6
Damien Buchet <damienbuchetdb@gmail.com>
2020-10-05 13:27:12 +0200

    Add moves

---
+++ Modeles/Paper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPShyFuMyClient.Modeles
+{
+    /// <summary>
+    /// Classe du papier
+    /// </summary>
+    public class Paper : Hand
+    {
+        #region Constructors
+        public Paper()
+        {
+
+        }
+        #endregion
+    }
+}
