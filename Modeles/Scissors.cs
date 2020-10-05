75d1edaf73da58536074e76c367b89f3ef547a2c
Damien Buchet <damienbuchetdb@gmail.com>
2020-10-05 13:33:25 +0200

    Re-add moves

---
+++ Modeles/Scissors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPShyFuMyClient.Modeles
+{
+    /// <summary>
+    /// Classe des ciseaux
+    /// </summary>
+    public class Scissors : Hand
+    {
+        #region Cosntructors
+        public Scissors()
+        {
+
+        }
+        #endregion
+    }
+}
