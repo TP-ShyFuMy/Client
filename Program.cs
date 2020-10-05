e8523acc182cf1c1efc0be2c09e93a79e161d2fc
Alexandre Breault <a.breault@iia-laval>
2020-10-05 14:26:41 +0200

    Username and move choice

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using TPShyFuMyClient.Modeles;
+using TPShyFuMyLibrary;
 
 namespace TPShyFuMyClient
 {
     class Program
     {
+        /// <summary>
+        /// Permet de choisir le pseudo du joueur
+        /// </summary>
+        /// <param name="user">Class User Library</param>
+        static void ChooseUsername(User user)
+        {
+            Console.WriteLine("Entrez votre pseudo :");
+            string userName = Console.ReadLine();
+            user.userName = userName;
+            Console.Clear();
+        }
+        static void ChoiceHand(User user)
+        {
+            Console.WriteLine(user.userName+" choisissez mouvement :");
+            Console.WriteLine(" Papier-> p");
+            Console.WriteLine(" Pierre-> r");
+            Console.WriteLine(" Ciseaux-> s");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Client!");
-            Scissors Scissors = new Scissors();
-            Console.WriteLine(Scissors.HandMove.ToString());
+            User user = new User();
+            ChooseUsername(user);
+            ChoiceHand(user);
+            Console.ReadLine();
+
         }
     }
 }
