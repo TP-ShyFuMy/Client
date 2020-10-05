8d3d92108cab858973c6562a0b11ea00b42f9707
Damien Buchet <damienbuchetdb@gmail.com>
2020-10-05 12:06:09 +0200

    Add hand class

---
+++ Hand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPShyFuMyClient
+{
+    /// <summary>
+    /// Mouvements de la main
+    /// </summary>
+    public abstract class Hand
+    {
+        #region Attributes
+        private string HandMove;
+        #endregion
+
+    }
+}
